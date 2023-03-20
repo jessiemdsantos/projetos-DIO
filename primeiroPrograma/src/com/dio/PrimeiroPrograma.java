f84e790a45d54d574e2a7dcd7ccf6c4d3dfc469b
jessiemdsantos <jessie.md.santos@gmail.com>
2023-03-20 02:46:56 -0300

    first commit

---
+++ primeiroPrograma/src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,18 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		System.out.println(gato);
+		
+		/*int a = 2;
+		int b = 4;
+		System.out.println("Hello, World! " + (a+b));
+*/
+	}
+
+}
