117c03fa7294b394f64bf918ae32097f1c95cd5c
jessiemdsantos <jessie.md.santos@gmail.com>
2023-04-21 20:30:46 -0300

    Spring Security

---
+++ SpringSecurity/dio-spring-security-jwt/src/test/java/dio/dio/spring/securit/jwt/DioSpringSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.dio.spring.securit.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioSpringSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
